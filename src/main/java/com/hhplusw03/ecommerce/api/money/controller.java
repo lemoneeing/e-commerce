ff2d77682494c6d3e6f3928aaa54a58999676699
jh <jh2two@gmail.com>
2024-04-04 21:12:37 +0900

    Fix: Money 관련 Controller 함수 @RequestParam 수정

--- src/main/java/com/hhplusw03/ecommerce/api/money/controller.java
+++ src/main/java/com/hhplusw03/ecommerce/api/money/controller.java
@@ -1,7 +1,6 @@
 package com.hhplusw03.ecommerce.api.money;
 
 import com.hhplusw03.ecommerce.api.money.dto.fakeUserWallet;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,16 +11,19 @@ public class controller {
     fakeUserWallet fakeWallet;
 
     // 사용자 Id 로 잔액 조회
-    @GetMapping("?userId={userId}")
+    @GetMapping
+    @ResponseBody
     public String showMoney(@RequestParam String userId){
+//    @GetMapping("/user/{userId}")
+//    public String showMoney(@PathVariable String userId){
         return "userId: " + userId +"\nmoney: " + fakeWallet.getMoney(userId);
     }
 
     // 사용자 Id 로 Money 충전
-    @GetMapping("/charge/?userId={userId}&moneyAmount={amount}")
+//    @GetMapping("/charge/?userId={userId}&moneyAmount={amount}")
+    @GetMapping("/charge")
     public String chargeMoney(@RequestParam String userId, @RequestParam Long amount){
         fakeWallet.addMoney(userId, amount);
         return "userId: " + userId +"\nmoney: " + fakeWallet.getMoney(userId);
     }
-
 }
