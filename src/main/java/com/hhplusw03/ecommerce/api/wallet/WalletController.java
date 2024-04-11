5793c1b4c1472e90de83fd8b908bfc2c35f9aff3
jh <jh2two@gmail.com>
2024-04-11 23:46:36 +0900

    Feature: createWallet() 구현
    - Response 생성 및 수정 방법 아직 모름...

--- src/main/java/com/hhplusw03/ecommerce/api/wallet/WalletController.java
+++ src/main/java/com/hhplusw03/ecommerce/api/wallet/WalletController.java
@@ -1,24 +1,31 @@
 package com.hhplusw03.ecommerce.api.wallet;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.hhplusw03.ecommerce.api.wallet.dto.response.ResponseDto;
-import com.hhplusw03.ecommerce.api.wallet.dto.response.WalletResponseDto;
+import com.hhplusw03.ecommerce.api.wallet.usecase.CreateWalletUseCase;
 import com.hhplusw03.ecommerce.api.wallet.usecase.DisplayWalletUseCase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.Map;
+
+@RestController
 @RequestMapping("/wallet")
 @RequiredArgsConstructor
 public class WalletController {
 
     private final DisplayWalletUseCase displayWalletUc;
+    private final CreateWalletUseCase createWalletUc;
+
+    @PostMapping("/new")
+    public ResponseDto createWallet(@RequestBody Map<String, String> requestBodyMap){
+
+        // Request body 에 "ueseId" 라는 key 가 없을 때 response 를 400 으로 주려면 어떻게...?
+        Long userId = Long.parseLong(requestBodyMap.get("userId"));
+        return createWalletUc.execute(userId);
+    }
 
     @GetMapping("/balance")
-    @ResponseBody
-    public ResponseDto showBalance(@RequestParam("user") Long userId){
+    public ResponseDto showBalance(@RequestParam Long userId){
         // 사용자 Id 로 잔액 조회
         return displayWalletUc.execute(userId);
     }
