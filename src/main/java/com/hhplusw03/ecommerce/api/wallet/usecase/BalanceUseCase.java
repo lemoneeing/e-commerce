eb6398e14ba8facc5fc1d7b2388009b2a2a8901a
jh <jh2two@gmail.com>
2024-04-15 23:22:07 +0900

    Feature: execute 의 반환 type 을 ResponseEntity<ResponseDto> 로 교체

---
+++ src/main/java/com/hhplusw03/ecommerce/api/wallet/usecase/BalanceUseCase.java
@@ -0,0 +1,30 @@
+package com.hhplusw03.ecommerce.api.wallet.usecase;
+
+import com.hhplusw03.ecommerce.api.wallet.dto.response.NotFoundUserResponseDto;
+import com.hhplusw03.ecommerce.api.wallet.dto.response.ResponseDto;
+import com.hhplusw03.ecommerce.api.wallet.dto.response.WalletResDto;
+import com.hhplusw03.ecommerce.domain.wallet.components.WalletReader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BalanceUseCase {
+    private final WalletReader walletReader;
+
+    // Wallet 의 상태를 보여줌.
+    public ResponseEntity<ResponseDto> execute(String userId){
+
+        Long ownerId = Long.parseLong(userId);
+        Long balance = this.walletReader.readBalanceByUserId(ownerId);
+
+        if (balance >= 0L) {
+            return ResponseEntity.status(HttpStatus.OK).body(new WalletResDto(ownerId, balance));
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new NotFoundUserResponseDto());
+        }
+    }
+}
