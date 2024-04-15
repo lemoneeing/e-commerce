e42a8a7c3fb8a82b26c3b3bab96e1acebac9833a
jh <jh2two@gmail.com>
2024-04-15 21:37:05 +0900

    Feature: Wallet 생성 요청에 Mapping 될 NewWalletReqDto 추가

---
+++ src/main/java/com/hhplusw03/ecommerce/api/wallet/dto/request/NewWalletReqDto.java
@@ -0,0 +1,13 @@
+package com.hhplusw03.ecommerce.api.wallet.dto.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewWalletReqDto {
+    private String userId;
+}
