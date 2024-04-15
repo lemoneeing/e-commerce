4fe1f3b159f4e40588431e84ad92b3c51b27c676
jh <jh2two@gmail.com>
2024-04-15 23:12:49 +0900

    Feature: WalletResponseDto -> WalletResDto 로 변경

--- src/main/java/com/hhplusw03/ecommerce/api/wallet/dto/response/WalletResDto.java
+++ src/main/java/com/hhplusw03/ecommerce/api/wallet/dto/response/WalletResDto.java
@@ -5,15 +5,15 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class WalletResponseDto implements ResponseDto{
+public class WalletResDto implements ResponseDto{
     /*
-    * Wallet 생성 응답에 Mapping
+    * Wallet 반환 응답
     */
 
     private String userId;
     private String balance;
 
-    public WalletResponseDto(Long userId, Long balance){
+    public WalletResDto(Long userId, Long balance){
         //DTO 의 멤버 변수 형변환은 어디서 이루어지는 것이 좋을까?
 
         this.userId = String.valueOf(userId);
