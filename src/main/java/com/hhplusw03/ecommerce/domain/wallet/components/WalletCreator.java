71fc5bd21ace0cbaa8f2fcd0acd5b6c628ab2d60
jh <jh2two@gmail.com>
2024-04-17 00:17:27 +0900

    Feature: 1) 반환 값 생성 시 walletCreator.createWallet() 을 활용하도록 수정 - Code Line 간결화 효과 2) Wallet 생성 확인 code 제거 - Test Code 가 이미 있으므로 중복이라고 판단.

--- src/main/java/com/hhplusw03/ecommerce/domain/wallet/components/WalletCreator.java
+++ src/main/java/com/hhplusw03/ecommerce/domain/wallet/components/WalletCreator.java
@@ -2,17 +2,20 @@ package com.hhplusw03.ecommerce.domain.wallet.components;
 
 import com.hhplusw03.ecommerce.domain.wallet.infrastructure.WalletCoreCreatorRepository;
 import com.hhplusw03.ecommerce.domain.wallet.models.Wallet;
-import lombok.RequiredArgsConstructor;
+import com.hhplusw03.ecommerce.domain.wallet.models.WalletDto;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class WalletCreator {
 
     private final WalletCoreCreatorRepository walletRepo;
 
-    public void createWalletByUserId(Long userId){
+    public WalletDto createWallet(Long userId){
         Wallet wallet = new Wallet(userId);
         walletRepo.create(wallet);
+
+        return wallet.toDto();
     }
 }
