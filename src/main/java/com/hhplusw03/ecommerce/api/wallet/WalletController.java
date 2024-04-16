c32b535309b0b7b1ac5343c9f3298335ca214992
jh <jh2two@gmail.com>
2024-04-17 00:43:41 +0900

    Feature: '지갑_충전' Test 를 통과할 수 있도록 chargeWallet() 수정

--- src/main/java/com/hhplusw03/ecommerce/api/wallet/WalletController.java
+++ src/main/java/com/hhplusw03/ecommerce/api/wallet/WalletController.java
@@ -1,7 +1,9 @@
 package com.hhplusw03.ecommerce.api.wallet;
 
+import com.hhplusw03.ecommerce.api.wallet.dto.request.ChargeReqDto;
 import com.hhplusw03.ecommerce.api.wallet.dto.request.WalletReqDto;
 import com.hhplusw03.ecommerce.api.wallet.dto.response.ResponseDto;
+import com.hhplusw03.ecommerce.api.wallet.usecase.ChargeUseCase;
 import com.hhplusw03.ecommerce.api.wallet.usecase.NewWalletUseCase;
 import com.hhplusw03.ecommerce.api.wallet.usecase.BalanceUseCase;
 
@@ -16,7 +18,7 @@ public class WalletController {
 
     private final NewWalletUseCase createWalletUc;
     private final BalanceUseCase balanceUc;
-//    private final ChargeUseCase chargeUc;
+    private final ChargeUseCase chargeUc;
 
     @PostMapping("/new")
     public ResponseEntity<ResponseDto> newWallet(@RequestBody WalletReqDto reqDto){
@@ -31,20 +33,10 @@ public class WalletController {
         return balanceUc.execute(reqDto.getUserId());
     }
 
-    // 사용자 Id 로 Money 충전
-//    @PatchMapping("/charge")
-//    public ResponseDto chargeWallet(@RequestBody Map<String, String> requestBodyMap){
-//        // 지갑 충전
-//        // Request body 에 "ueseId" 라는 key 가 없을 때 response 를 400 으로 주려면 어떻게...?
-//        Long userId = Long.parseLong(requestBodyMap.get("userId"));
-//        Long amount = Long.parseLong(requestBodyMap.get("amount"));
-//        return chargeUc.execute(userId, amount);
-//    }
-//    @PatchMapping("/charge")
-//    public ResponseDto chargeWallet(@RequestBody ChargeData chargeData) {
-//        //지갑 충전
-//        Long userId = Long.parseLong(chargeData.getUserId());
-//        Long amount = Long.parseLong(chargeData.getAmount());
-//        return chargeUc.execute(userId, amount);
-//    }
+    // 사용자 Id 를 갖는 Wallet 에 Money 충전
+    @PatchMapping("/charge")
+    public ResponseEntity<ResponseDto> chargeWallet(@RequestBody ChargeReqDto chargeDto) {
+        //지갑 충전
+        return chargeUc.execute(chargeDto.getUserId(), chargeDto.getAmount());
+    }
 }
