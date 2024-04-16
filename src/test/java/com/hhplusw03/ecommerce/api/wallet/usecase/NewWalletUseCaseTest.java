cfccb41de9c3efee7389f8f2652f1abc79e02d50
jh <jh2two@gmail.com>
2024-04-17 00:07:37 +0900

    Test: '지갑_생성_서비스' Test 추가

---
+++ src/test/java/com/hhplusw03/ecommerce/api/wallet/usecase/NewWalletUseCaseTest.java
@@ -0,0 +1,46 @@
+package com.hhplusw03.ecommerce.api.wallet.usecase;
+
+import com.hhplusw03.ecommerce.api.wallet.dto.response.ResponseDto;
+import com.hhplusw03.ecommerce.domain.wallet.components.WalletCreator;
+import com.hhplusw03.ecommerce.domain.wallet.components.WalletReader;
+import com.hhplusw03.ecommerce.domain.wallet.models.Wallet;
+import org.assertj.core.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.http.ResponseEntity;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+@Import({NewWalletUseCase.class, WalletCreator.class})
+public class NewWalletUseCaseTest {
+    @MockBean
+    WalletCreator creator;
+    @MockBean
+    WalletReader reader;
+
+    @Autowired
+    NewWalletUseCase newWalletUc;
+
+    @Test
+    public void 지갑_생성_서비스(){
+        Long userId = 1L;
+        when(reader.checkWalletExistByUserId(userId)).thenReturn(false);
+        when(creator.createWallet(userId)).thenReturn(new Wallet(userId).toDto());
+
+        ResponseEntity<ResponseDto> walletResDto = newWalletUc.execute(String.valueOf(userId));
+
+        Assertions.assertThat(walletResDto).isInstanceOf(ResponseEntity.class);
+        Assertions.assertThat(walletResDto.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(walletResDto.getBody()).hasFieldOrPropertyWithValue("balance", "0");
+
+        verify(reader, times(1)).checkWalletExistByUserId(userId);
+        verify(creator, times(1)).createWallet(userId);
+    }
+}
