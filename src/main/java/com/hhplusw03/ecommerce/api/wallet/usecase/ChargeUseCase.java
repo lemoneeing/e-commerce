4b84ed7915e0f56c6402875b6b6717a51e759d44
jh <jh2two@gmail.com>
2024-04-17 22:30:37 +0900

    Feature: 최소 금액(10000원) 미만은 ErrorResDto 를 반환하도록 구현

--- src/main/java/com/hhplusw03/ecommerce/api/wallet/usecase/ChargeUseCase.java
+++ src/main/java/com/hhplusw03/ecommerce/api/wallet/usecase/ChargeUseCase.java
@@ -1,11 +1,13 @@
 package com.hhplusw03.ecommerce.api.wallet.usecase;
 
+import com.hhplusw03.ecommerce.api.wallet.dto.response.ErrorResDto;
 import com.hhplusw03.ecommerce.api.wallet.dto.response.ResponseDto;
-import com.hhplusw03.ecommerce.api.wallet.dto.response.WalletResponseDto;
-import com.hhplusw03.ecommerce.domain.wallet.components.WalletCreator;
+import com.hhplusw03.ecommerce.api.wallet.dto.response.WalletResDto;
 import com.hhplusw03.ecommerce.domain.wallet.components.WalletModifier;
 import com.hhplusw03.ecommerce.domain.wallet.components.WalletReader;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,12 +16,20 @@ public class ChargeUseCase {
     private final WalletReader walletReader;
     private final WalletModifier walletModifier;
 
-    public ResponseDto execute(Long userId, Long amount){
-        Long initBal = walletReader.readBalanceByUserId(userId);
+    public ResponseEntity<ResponseDto> execute(String userId, String amount){
+
+        Long ownerId = Long.parseLong(userId);
+        Long amount2Charge = Long.parseLong(amount);
+
+        // 10000 원 미만 금액은 충전 불가
+        if (amount2Charge < 10000) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResDto("Too little amount to charge."));
+
+        Long initBal = walletReader.readBalanceByUserId(ownerId);
+
 
         // 직접 Wallet Class 를 사용한 것은 아니지만, readWalletByUserId 의 반환 Type 이 Wallet 일 때, 정말 의존성이 없다고 할 수 있는 것인가?
-        walletModifier.modifyBalanceByUserIdAndAmount(walletReader.readWalletByUserId(userId), initBal + amount);
+        walletModifier.modifyBalanceByUserIdAndAmount(walletReader.readWalletByUserId(ownerId), initBal + amount2Charge);
 
-        return new WalletResponseDto(userId, walletReader.readBalanceByUserId(userId));
+        return ResponseEntity.status(HttpStatus.OK).body(new WalletResDto(ownerId, walletReader.readBalanceByUserId(ownerId)));
     }
 }
