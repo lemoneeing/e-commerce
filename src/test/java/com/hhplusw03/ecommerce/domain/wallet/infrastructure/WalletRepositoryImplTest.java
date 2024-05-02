836a4e8e2cc56ebd1070cdfd3def6eee86de9070
jh <jh2two@gmail.com>
2024-05-02 23:06:52 +0900

    Test: [WalletRepositoryImplTest] '지갑_충전()' 추가, '지갑_생성()' 과 비슷한 이유로 Test fail

--- src/test/java/com/hhplusw03/ecommerce/domain/wallet/infrastructure/WalletRepositoryImplTest.java
+++ src/test/java/com/hhplusw03/ecommerce/domain/wallet/infrastructure/WalletRepositoryImplTest.java
@@ -10,15 +10,17 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
-@Import({WalletRepositoryImpl.class, WalletJpaRepository.class})
-class WalletRepositoryImplImplTest {
+@Import({WalletRepositoryImpl.class})
+class WalletRepositoryImplTest {
     @MockBean
     private WalletJpaRepository jpaRepo;
     @Autowired
-    private WalletRepositoryImpl coreRepo;// = new WalletRepositoryImpl(jpaRepo);
+    private WalletRepositoryImpl coreRepo;
 
 //    @BeforeEach
 //    void setUp() {
@@ -32,9 +34,24 @@ class WalletRepositoryImplImplTest {
         Long userId = 4L;
 
         WalletEntity wallet = new WalletEntity(userId);
-        when(jpaRepo.save(wallet)).thenReturn(wallet);
+        when(this.jpaRepo.save(new WalletEntity(userId))).thenReturn(wallet);
 
-        WalletEntity savedWallet = coreRepo.saveWallet(userId); // 반환 값이 null 인 이유 ????
+        WalletEntity savedWallet = this.coreRepo.saveWallet(userId); // 반환 값이 null 인 이유 ????
         Assertions.assertThat(savedWallet).isEqualTo(wallet);
+    }
+
+    @Test
+    public void 지갑_충전(){
+        Long userId = 1L;
+
+        WalletEntity wallet = new WalletEntity(userId);
+        when(this.jpaRepo.findByUserId(userId)).thenReturn(Optional.of(wallet));
+
+        Long newBalance = 50000L;
+        wallet.setBalance(newBalance);
+        when(this.jpaRepo.save(wallet)).thenReturn(wallet);
+
+        WalletEntity chargedWallet = this.coreRepo.modifyBalance(wallet, newBalance); // 여기도 역시나 null 이 반환... why?!?!?
+        Assertions.assertThat(chargedWallet).isEqualTo(wallet);
     }
 }
