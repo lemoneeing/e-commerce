a85669f93becc58c4a8d2ca41ca8c96d0e567549
jh <jh2two@gmail.com>
2024-05-03 00:38:53 +0900

    Test: [ChargeWalletUseCase] execute() 구현

--- src/main/java/com/hhplusw03/ecommerce/api/wallet/usecase/ChargeWalletUseCase.java
+++ src/main/java/com/hhplusw03/ecommerce/api/wallet/usecase/ChargeWalletUseCase.java
@@ -1,17 +1,25 @@
 package com.hhplusw03.ecommerce.api.wallet.usecase;
 
+import com.hhplusw03.ecommerce.api.responseDto.ResponseDto;
+import com.hhplusw03.ecommerce.api.wallet.dto.response.WalletResDto;
+import com.hhplusw03.ecommerce.domain.wallet.application.WalletDto;
+import com.hhplusw03.ecommerce.domain.wallet.application.WalletResult;
+import com.hhplusw03.ecommerce.domain.wallet.application.WalletService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChargeWalletUseCase {
-//    private final WalletReader walletReader;
-//    private final WalletModifier walletModifier;
+    private final WalletService walletSvc;
+    @Autowired
+    public ChargeWalletUseCase(WalletService svc){
+        this.walletSvc = svc;
+    }
 
-//    public ResponseEntity<ResponseDto> execute(String userId, String amount){
-//
-//        Long ownerId = Long.parseLong(userId);
-//        Long amount2Charge = Long.parseLong(amount);
-//
+    public ResponseDto execute(String userId, String amount){
+
 //        // 최소 금액 (10000 원) 미만은 충전 불가
 //        if (amount2Charge < 10000) {
 //            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResDto("Too little amount to charge."));
@@ -21,5 +29,8 @@ public class ChargeWalletUseCase {
 //        Long initBal = walletReader.readBalanceByUserId(ownerId);
 //        WalletDto walletDto = walletModifier.modifyBalance(ownerId, initBal + amount2Charge);
 //        return ResponseEntity.status(HttpStatus.OK).body(new WalletResDto(walletDto.getUserId(), walletDto.getBalance()));
-//    }
+
+//        WalletResult wallet = this.walletSvc.readWallet(userId);
+        return new WalletResDto((WalletDto) this.walletSvc.chargeWallet(userId, amount));
+    }
 }
