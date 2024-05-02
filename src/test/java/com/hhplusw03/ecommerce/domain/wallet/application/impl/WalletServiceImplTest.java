07fb00e0fcf3b0b36ba002d24ee6dbd5c137377a
jh <jh2two@gmail.com>
2024-05-03 00:04:51 +0900

    Test: [WalletServiceImplTest] 지갑_충전_서비스() 추가

--- src/test/java/com/hhplusw03/ecommerce/domain/wallet/application/impl/WalletServiceImplTest.java
+++ src/test/java/com/hhplusw03/ecommerce/domain/wallet/application/impl/WalletServiceImplTest.java
@@ -2,6 +2,7 @@ package com.hhplusw03.ecommerce.domain.wallet.application.impl;
 
 import com.hhplusw03.ecommerce.domain.wallet.application.WalletDto;
 import com.hhplusw03.ecommerce.domain.wallet.application.WalletResult;
+import com.hhplusw03.ecommerce.domain.wallet.infrastructure.WalletRepositoryImpl;
 import com.hhplusw03.ecommerce.domain.wallet.models.WalletEntity;
 import com.hhplusw03.ecommerce.domain.wallet.repository.WalletRepository;
 import org.assertj.core.api.Assertions;
@@ -15,11 +16,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
-@Import({WalletServiceImpl.class, WalletRepository.class})
+@Import({WalletServiceImpl.class, WalletRepositoryImpl.class})
 class WalletServiceImplTest {
 
     @MockBean
-    WalletRepository dao;
+    WalletRepositoryImpl repo;
     @Autowired
     WalletServiceImpl svc;
 
@@ -28,7 +29,7 @@ class WalletServiceImplTest {
         String userId = "1";
 
         WalletEntity walletEntity = new WalletEntity(Long.parseLong(userId));
-        when(dao.saveWallet(Long.parseLong(userId))).thenReturn(walletEntity);
+        when(repo.saveWallet(Long.parseLong(userId))).thenReturn(walletEntity);
 
         WalletResult walletRes = svc.createWallet(userId);
 
@@ -37,7 +38,7 @@ class WalletServiceImplTest {
         Assertions.assertThat(((WalletDto)walletRes).getUserId()).isEqualTo(Long.parseLong(userId));
         Assertions.assertThat(((WalletDto)walletRes).getBalance()).isEqualTo(0L);
 
-        verify(dao, times(1)).saveWallet(Long.parseLong(userId));
+        verify(repo, times(1)).saveWallet(Long.parseLong(userId));
     }
 
 //    @Test
@@ -59,7 +60,7 @@ class WalletServiceImplTest {
 
         // 조회 시험
         WalletEntity walletEntity = new WalletEntity(Long.parseLong(userId), Long.parseLong(initBalance));
-        when(dao.readWallet(Long.parseLong(userId))).thenReturn(walletEntity);
+        when(repo.readWallet(Long.parseLong(userId))).thenReturn(walletEntity);
 
         WalletResult walletRes = svc.readWallet(userId);
 
@@ -67,29 +68,38 @@ class WalletServiceImplTest {
         Assertions.assertThat(((WalletDto)walletRes).getUserId()).isEqualTo(Long.parseLong(userId));
         Assertions.assertThat(((WalletDto)walletRes).getBalance()).isEqualTo(Long.parseLong(initBalance));
 
-        verify(dao, times(1)).readWallet(Long.parseLong(userId));
+        verify(repo, times(1)).readWallet(Long.parseLong(userId));
     }
 
     @Test
     public void 지갑_충전_서비스(){
-        String userId = "1";
-        String initBalance = "0";
-        //지갑 데이터 생성
-        when(dao.saveWallet(Long.parseLong(userId))).thenReturn(new WalletEntity(Long.parseLong(userId), Long.parseLong(initBalance)));
-        svc.createWallet(userId);
+//        String userId = "1";
+//        String initBalance = "0";
+//        //지갑 데이터 생성
+//        when(repo.saveWallet(Long.parseLong(userId))).thenReturn(new WalletEntity(Long.parseLong(userId), Long.parseLong(initBalance)));
+//        svc.createWallet(userId);
 
-        String amountToCharge = "50000";
+//        String amountToCharge = "50000";
+
+        Long userId = 1L;
+        Long initBalance = 0L;
+        Long amountToCharge = 50000L;
         // 충전 시험
-        WalletEntity walletEntity = new WalletEntity(Long.parseLong(userId), Long.parseLong(initBalance));
-//        when(dao.modifyWallet(Long.parseLong(userId))).thenReturn(walletEntity);
+        WalletEntity walletEntity = new WalletEntity(userId, initBalance);
+        when(repo.readWallet(userId)).thenReturn(walletEntity);
 
-        WalletResult walletRes = svc.readWallet(userId);
+//        walletEntity.setBalance(initBalance + amountToCharge);
+        WalletEntity newWalletEntity = new WalletEntity(userId, amountToCharge);
+        when(repo.modifyBalance(walletEntity, initBalance + amountToCharge)).thenReturn(newWalletEntity);
+
+        WalletResult walletRes = svc.chargeWallet(String.valueOf(userId), String.valueOf(amountToCharge));
 
         Assertions.assertThat(walletRes).isInstanceOf(WalletDto.class);
-        Assertions.assertThat(((WalletDto)walletRes).getUserId()).isEqualTo(Long.parseLong(userId));
-        Assertions.assertThat(((WalletDto)walletRes).getBalance()).isEqualTo(Long.parseLong(initBalance));
+        Assertions.assertThat(((WalletDto)walletRes).getUserId()).isEqualTo(userId);
+        Assertions.assertThat(((WalletDto)walletRes).getBalance()).isEqualTo(amountToCharge);
 
-        verify(dao, times(1)).readWallet(Long.parseLong(userId));
+        verify(repo, times(1)).readWallet(userId);
+        verify(repo, times(1)).modifyBalance(walletEntity, initBalance + amountToCharge);
     }
 //    @Test
 //    public void 존재하지_않는_사용자의_잔액_조회_서비스_실패(){ // -> 잔액 조회 서비스의 실패는 결국 DAP 에서 findByUserID() 가 Error 를 발생시켜야 함. 이 경우, service 시험 보다는 repository 시험이 더 적절?
