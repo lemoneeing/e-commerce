9bed9b4abaa0e72778ad8a7e525fcb49b0e4bff8
jh <jh2two@gmail.com>
2024-05-03 00:21:15 +0900

    Test: [WalletControllerTest] 지갑_잔액_조회() 추가

--- src/test/java/com/hhplusw03/ecommerce/api/wallet/WalletControllerTest.java
+++ src/test/java/com/hhplusw03/ecommerce/api/wallet/WalletControllerTest.java
@@ -4,6 +4,7 @@ import com.hhplusw03.ecommerce.api.responseDto.ResponseDto;
 import com.hhplusw03.ecommerce.api.wallet.dto.request.WalletReqDto;
 import com.hhplusw03.ecommerce.api.wallet.dto.response.WalletResDto;
 import com.hhplusw03.ecommerce.api.wallet.usecase.CreateWalletUsecase;
+import com.hhplusw03.ecommerce.api.wallet.usecase.ShowWalletUseCase;
 import com.hhplusw03.ecommerce.domain.wallet.application.WalletDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -22,7 +23,9 @@ import static org.mockito.Mockito.*;
 @Import({WalletController.class, CreateWalletUsecase.class})
 class WalletControllerTest {
     @MockBean
-    CreateWalletUsecase createWalletUsecase;
+    CreateWalletUsecase createWalletUseCase;
+    @MockBean
+    ShowWalletUseCase showWalletUseCase;
 
     @Autowired
     WalletController controller;
@@ -32,7 +35,7 @@ class WalletControllerTest {
         String userId = "1";
 
         WalletDto walletDto = new WalletDto(Long.parseLong(userId), 0L);
-        when(createWalletUsecase.execute(userId)).thenReturn(new WalletResDto(walletDto));
+        when(createWalletUseCase.execute(userId)).thenReturn(new WalletResDto(walletDto));
 
         ResponseEntity<ResponseDto> walletResDto = controller.newWallet(new WalletReqDto(userId));
 
@@ -41,6 +44,22 @@ class WalletControllerTest {
         Assertions.assertThat(((WalletResDto)walletResDto.getBody()).getUserId()).isEqualTo(userId);
         Assertions.assertThat(((WalletResDto)walletResDto.getBody()).getBalance()).isEqualTo("0");
 
-        verify(createWalletUsecase, times(1)).execute(userId);
+        verify(createWalletUseCase, times(1)).execute(userId);
+    }
+    @Test
+    public void 지갑_잔액_조회(){
+        String userId = "1";
+
+        WalletDto walletDto = new WalletDto(Long.parseLong(userId), 0L);
+        when(showWalletUseCase.execute(userId)).thenReturn(new WalletResDto(walletDto));
+
+        ResponseEntity<ResponseDto> walletResDto = controller.showWallet(new WalletReqDto(userId));
+
+        Assertions.assertThat(walletResDto).isInstanceOf(ResponseEntity.class);
+        Assertions.assertThat(walletResDto.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(((WalletResDto)walletResDto.getBody()).getUserId()).isEqualTo(userId);
+        Assertions.assertThat(((WalletResDto)walletResDto.getBody()).getBalance()).isEqualTo("0");
+
+        verify(showWalletUseCase, times(1)).execute(userId);
     }
 }
