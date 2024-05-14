ce9d2fab7c5479b0d229abec2a7fb2d0108f9e1f
jh <jh2two@gmail.com>
2024-05-14 23:20:33 +0900

    Feature: [ItemServiceImpl->ItemService] checkStock() 구현 완료 -> Test passed.

--- src/main/java/com/hhplusw03/ecommerce/domain/item/application/ItemService.java
+++ src/main/java/com/hhplusw03/ecommerce/domain/item/application/ItemService.java
@@ -2,6 +2,6 @@ package com.hhplusw03.ecommerce.domain.item.application;
 
 public interface ItemService{
     public ItemResult registerItem(String name, Long price, String seller, Long stock);
-
     public ItemResult readItem(Long itemId);
+    public Boolean checkStock(Long itemId);
 }
